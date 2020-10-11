40d0890125e6cd70c82b90a441f8f51f9e2b3e39
Reja <zillirsk@gmail.com>
2020-10-12 01:07:40 +0600

    Relationship Created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -4,7 +4,11 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
-    //
+    public function review(){
+
+       return $this->hasMany(Review::class);
+    }
 }
